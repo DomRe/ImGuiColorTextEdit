6b87d3f9517a3454733103ee9e3864c951ea3d84
Johan Goossens <johan@cox.net>
2022-01-10 15:34:21 -0500

    Remove redundant assignment #134

--- TextEditor.cpp
+++ TextEditor.cpp
@@ -2391,7 +2391,7 @@ void TextEditor::ColorizeInternal()
 							withinSingleLineComment = true;
 						}
 
-						inComment = inComment = (commentStartLine < currentLine || (commentStartLine == currentLine && commentStartIndex <= currentIndex));
+						inComment = (commentStartLine < currentLine || (commentStartLine == currentLine && commentStartIndex <= currentIndex));
 
 						line[currentIndex].mMultiLineComment = inComment;
 						line[currentIndex].mComment = withinSingleLineComment;
